53c77d81f36c09b8aa484e1093454c9e6dfa16c4
tommylee <junghun1013@icloud.com>
2023-05-11 15:50:00 +0900

    Saving

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import *
+from markupsafe import escape
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/users/<username>')
+def get_user(username) : return 'User %s' % escape(username)
+
+@app.route('/posts/<int:post_id>')
+def get_post(post_id) : return 'Post %d' % post_id
+@app.route('/uuid/<uuid:uuid>')
+def get_uuid(uuid) : return 'uuid %s' % escape(uuid)
